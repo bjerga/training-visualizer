767629237afc578823a54c0bb5c8480f5173cbd0
mikaelbj <mikaelbj@stud.ntnu.no>
2016-10-26 17:00:16 +0200

    Add requirements to username and password

--- visualizer/helpers.py
+++ visualizer/helpers.py
@@ -5,16 +5,32 @@ from .models import User
 
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+USERNAME_MINIMUM_LENGTH = 6
+USERNAME_MAXIMUM_LENGTH = 20
+PASSWORD_MINIMUM_LENGTH = 8
 
 
 def valid_username(username):
-	# TODO: use regex operation re.search or re.match
-	return True
+	# checks if password only contains letters (capitalized or not), numbers and underscores,
+	# and that it is has a length in between the predetermined minimum and maximum length
+	if re.match('^[\w]*$', username) and USERNAME_MINIMUM_LENGTH <= len(username) <= USERNAME_MAXIMUM_LENGTH:
+		return True
+	return False
 
 
 def valid_password(password):
-	# TODO: use regex operation re.search or re.match
-	return True
+	# checks if password only contains letters (capitalized or not) and numbers,
+	# and that it is at least as long the predetermined minimum length,
+	# and that it contains at least 1 capitalized letter,
+	# and that it contains at least 1 non-capitalized letter,
+	# and that it contains at least 2 numbers
+	if re.match('^[\w]*$', password) and \
+					len(password) >= PASSWORD_MINIMUM_LENGTH and \
+					len(re.findall('[A-Z]', password)) > 0 and \
+					len(re.findall('[a-z]', password)) > 0 and \
+					len(re.findall('[0-9]', password)) > 1:
+		return True
+	return False
 
 
 def unique_username(username):

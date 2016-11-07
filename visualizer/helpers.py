e12dd2c9b4979a38b10f5f50713b5112cadb4296
mikaelbj <mikaelbj@stud.ntnu.no>
2016-11-07 20:26:29 +0100

    Add plotting for toy python writer

--- visualizer/helpers.py
+++ visualizer/helpers.py
@@ -1,7 +1,11 @@
 import re
 import subprocess as sub
+import matplotlib.pyplot as plt
+from time import sleep
+
 from flask import abort
 from flask_login import current_user
+
 from .models import User
 
 
@@ -58,17 +62,81 @@ def get_form_errors(form):
 	return form_errors
 
 
-def run_python_shell(file_path):
+def run_python_shell(file_path, shared_bool):
 	if file_path:
-		# rel_path = 'temp_uploads\liste.py'
-		# print('\n\nFile found at path: %s\n\n' % file.path)
-		# print('\n\nTry using command: %s\n\n' % ('python ' + file.path))
-		# cmd = 'python %s' % file.path
-		# cmd = 'D: & cd ' + os.path.dirname(__file__) + ' & python ' + rel_path
-		# cmd = '''cd ..'''
-		# print('Try command:', cmd)
+		
+		print('\nSubprocess started\n')
+		
+		# run program via command line
 		sub.run('python ' + file_path)
-		# sub.run('cd ' + os.path.dirname(__file__))
-		# sub.run('python ' + rel_path)
+		
+		print('\nSubprocess finished\n')
 	else:
 		print('\n\nNo file found\n\n')
+	
+	# mark via shared boolean that process is no longer writing
+	shared_bool.value = False
+	
+	# make sure process ends (not certain this is needed)
+	return
+
+
+def plot_accuracy_error(file_path, shared_bool):
+
+	# create file path for plots
+	plot_file_path = file_path.replace('programs', 'plots').replace('.py', '_result.txt')
+	print('\nPlot file path: %s\n' % plot_file_path)
+	
+	accuracy_errors = []
+	num = 0
+	# if other process is writing
+	while shared_bool.value:
+		sleep(6)
+		
+		# copy old errors
+		old_accuracy_errors = accuracy_errors[:]
+		
+		try:
+			accuracy_errors = read_plot_file(plot_file_path)
+		# if file has not been made yet, sleep and try again
+		except FileNotFoundError:
+			print('\nFile not found\n')
+			continue
+			
+		print('Errors so far:', accuracy_errors)
+		# if new errors read, plot and save accuracy error so far
+		if len(accuracy_errors) != len(old_accuracy_errors):
+			# plt.figure(figsize=(20, 10))
+			plt.plot(accuracy_errors, 'r-', label='Error')
+			plt.legend(loc='upper right')
+			plt.title('Accuracy Error Over Epochs')
+			plt.xlabel('Epoch')
+			plt.ylabel('Accuracy Error')
+			
+			# hard-coded
+			plt.xlim([0, 10])
+			plt.ylim([0, 10])
+			
+			plt.savefig(plot_file_path.replace('.txt', '_%d.png' % num))
+			# plt.show()
+			plt.clf()
+			plt.close()
+			
+			print('\nSaved accuracy error plot as no. %d\n' % num)
+			num += 1
+			
+	print('\nPlotting done\n')
+		
+	# make sure process ends (not certain this is needed)
+	return
+
+
+def read_plot_file(plot_file_path):
+	
+	accuracy_errors = []
+	with open(plot_file_path, 'r') as f:
+		for line in f:
+			if line != '':
+				accuracy_errors.append(float(line))
+	
+	return accuracy_errors

c4d6a6589f4eb5aca53b699581b8d17e57530e75
mikaelbj <mikaelbj@stud.ntnu.no>
2017-01-26 18:00:00 +0100

    Major rework of deep visualization technique attempt

--- test_code/deep_dream/deep_vis_keras.py
+++ test_code/deep_dream/deep_vis_keras.py
@@ -1,89 +1,80 @@
 import numpy as np
 
 import scipy.misc
-import time
-import os
-import h5py
+from time import time
 from scipy.ndimage.filters import gaussian_filter
+from os import mkdir
+from os.path import dirname, join
 
 from keras.models import Sequential
-from keras.layers import Input, Convolution2D, ZeroPadding2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.layers import Dense
 from keras import backend as K
 from keras.applications.vgg16 import VGG16
 
 # VGG16 mean values
-MEAN_VALUES = np.array([103.939, 116.779, 123.68]).reshape((3, 1, 1))
+MEAN_VALUES = np.array([103.939, 116.779, 123.68])
 
-# path to the model weights file.
-weights_path = 'vgg16_weights.h5'
+# Update coefficient
+learning_rate = 1000.
+
+# how many times we update image
+no_of_iterations = 500
+
+# specify L2-decay
+# used to prevent a small number of extreme pixel values from dominating the output image
+l2_decay = 0.0001
+
+# specify frequency of blurring and standard deviation for kernel for Gaussian blur
+# used to penalize high frequency information in the output image
+blur_every = 4
+# standard deviation values between 0.0 and 0.3 work poorly, according to yosinski
+# blur_std = 0.3
+blur_std = 1.0
+
+# specify value percentile limit
+# used to induce sparsity by setting pixels with small absolute value to 0
+value_percentile = 0
+
+# specify norm percentile limit
+# used to induce sparsity by setting pixels with small norm to 0
+norm_percentile = 0
+
+# specify contribution percentile limit
+# used to induce sparsity by setting pixels with small contribution to 0
+contribution_percentile = 0
+
+# specify absolute contribution percentile limit
+# used to induce sparsity by setting pixels with small absolute contribution to 0
+abs_contribution_percentile = 0
+
+# choose whether to include regularization
+regularize = True
 
 
 # util function to convert a tensor into a valid image
-def deprocess(x):
+def deprocess(vis_tensor, color_axis):
 
-    x += MEAN_VALUES.transpose((1, 2, 0))  # Add VGG16 mean values
+    # remove batch dimension, and alter color axis accordingly
+    img = vis_tensor[0]
+    color_axis -= 1
 
-    # x = x[::-1, :, :]  # Change from BGR to RGB
-    # x = x.transpose((1, 2, 0))  # Change from (Channel,Height,Width) to (Height,Width,Channel)
+    # create shape with correct color dimension
+    new_shape = [1, 1, 1]
+    new_shape[color_axis] = 3
 
-    x = np.clip(x, 0, 255).astype('uint8')  # clip in [0;255] and convert to int
-    return x
+    # add VGG16 mean values
+    img += MEAN_VALUES.reshape(new_shape)
 
+    if color_axis == 0:
+        # alter dimensions from (color, height, width) to (height, width, color)
+        img = img.transpose((1, 2, 0))
 
-# Creates a VGG16 model and load the weights if available
-# (see https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3)
-def VGG_16(w_path=None):
-    vgg_model = Sequential()
-    vgg_model.add(ZeroPadding2D((1, 1), input_shape=(3, 224, 224)))
-    vgg_model.add(Convolution2D(64, 3, 3, activation='relu'))
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(64, 3, 3, activation='relu'))
-    vgg_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(128, 3, 3, activation='relu'))
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(128, 3, 3, activation='relu'))
-    vgg_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(256, 3, 3, activation='relu'))
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(256, 3, 3, activation='relu'))
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(256, 3, 3, activation='relu'))
-    vgg_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    vgg_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    vgg_model.add(ZeroPadding2D((1, 1)))
-    vgg_model.add(Convolution2D(512, 3, 3, activation='relu'))
-    vgg_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    vgg_model.add(Flatten())
-    vgg_model.add(Dense(4096, activation='relu'))
-    vgg_model.add(Dropout(0.5))
-    vgg_model.add(Dense(4096, activation='relu'))
-    vgg_model.add(Dropout(0.5))
-    vgg_model.add(Dense(1000, activation='linear'))  # avoid softmax (see Simonyan 2013)
-
-    if w_path:
-        vgg_model.load_weights(w_path)
-
-    return vgg_model
+    img = np.clip(img, 0, 255).astype('uint8')  # clip in [0;255] and convert to int
+    
+    return img
 
 
-def built_in_vgg_16():
+def create_model():
     base_model = VGG16(include_top=True, weights='imagenet')
 
     vgg_model = Sequential()
@@ -104,111 +95,255 @@ def built_in_vgg_16():
     return vgg_model
 
 
-# Creates the VGG models and loads weights
-# model = VGG_16(weights_path)
-model = built_in_vgg_16()
+def save_visualization(model_input_shape, kept_images):
+    
+    # define output path and make folder
+    output_path = join(dirname(__file__), 'output')
+    try:
+        mkdir(output_path)
+    except FileExistsError:
+        # folder exists, which is what we wanted
+        pass
 
-# Specify input and output of the network
-input_img = model.layers[0].input
-layer_output = model.layers[-1].output
+    # Compute the size of the grid
+    n = int(np.ceil(np.sqrt(len(kept_images))))
+    
+    # build a black picture with enough space for the kept_images
+    # img_height = model.input_shape[2]
+    # img_width = model.input_shape[3]
+    img_height = model_input_shape[1]
+    img_width = model_input_shape[2]
+    margin = 5
+    height = n * img_height + (n - 1) * margin
+    width = n * img_width + (n - 1) * margin
+    stitched_res = np.zeros((height, width, 3))
+    
+    # fill the picture with our saved filters
+    for i in range(n):
+        for j in range(n):
+            if len(kept_images) <= i * n + j:
+                break
+            img, loss = kept_images[i * n + j]
+            stitched_res[(img_height + margin) * i: (img_height + margin) * i + img_height,
+            (img_width + margin) * j: (img_width + margin) * j + img_width, :] = img
+    
+    # save the resulting image to disk
+    if regularize:
+        image_name = 'regularized_%d' % time()
+    else:
+        image_name = 'vanilla_%d' % time()
 
-# List of the generated images after learning
-kept_images = []
+    scipy.misc.toimage(stitched_res, cmin=0, cmax=255).save(join(output_path, image_name + '.png'))
+    # Do not use scipy.misc.imsave because it will normalize the image pixel value between 0 and 255
 
-# Update coefficient
-learning_rate = 1000.
+    # also save a txt-file containing information about creation environment
+    with open(join(output_path, image_name + '_info.txt'), 'a') as f:
+        f.write('Learning rate: %f\n' % learning_rate)
+        f.write('Number of iterations: %d\n' % no_of_iterations)
+        if regularize:
+            f.write('L2-decay: %f\n' % l2_decay)
+            f.write('Blur every and std: %d & %f\n' % (blur_every, blur_std))
+            f.write('Value percentile: %d\n' % value_percentile)
+            f.write('Norm percentile: %d\n' % norm_percentile)
+            f.write('Contribution percentile: %d\n' % contribution_percentile)
+            f.write('Abs. contribution percentile: %d\n' % abs_contribution_percentile)
 
-# how many times we update image
-no_of_iterations = 500
 
-# specify L2-decay
-l2_decay = 0.0001
-
-# specify frequency of blurring and standard deviation for kernel for Gaussian blur
-blur_freq = 4
-blur_std = 0.3
-
-# choose whether to include regularization
-regularize = True
-
-for class_index in [130, 351, 736, 850]:  # 130 flamingo, 351 hartebeest, 736 pool table, 850 teddy bear
-    print('Processing filter %d' % class_index)
-    start_time = time.time()
-
+def get_gradient_function(model_input, model_output, class_index):
+    
     # The loss is the activation of the neuron for the chosen class
-    loss = layer_output[0, class_index]
-
+    loss = model_output[0, class_index]
+    
     # we compute the gradient of the input picture w.r.t. this loss
-    grads = K.gradients(loss, input_img)[0]
-
+    grads = K.gradients(loss, model_input)[0]
+    
     # this function returns the loss and grads given the input picture
     # also add a flag to disable the learning phase (in our case dropout)
-    iterate = K.function([input_img, K.learning_phase()], [loss, grads])
+    return K.function([model_input, K.learning_phase()], [loss, grads])
 
+
+def create_initial_image(model_input_shape):
+    
     np.random.seed(1337)  # for reproducibility
     # we start from a gray image with some random noise
+    
+    # return a random, intial image, and the color axis of the image
+    # add (1,) for batch axis
+    return np.random.normal(0, 10, (1,) + model_input_shape[1:]), model_input_shape.index(3)
 
-    input_img_data = np.random.normal(0, 10, (1,) + model.input_shape[1:])  # (1,) for batch axis
 
-    # we run gradient ascent for 1000 steps
-    # for i in range(1000):
-    if class_index == 130:
-        for i in range(no_of_iterations):
-            # input 0 for test phase
-            loss_value, grads_value = iterate([input_img_data, 0])
+def ensemble_regularization(input_img_data, grads_value, iteration, color_axis):
+    
+    # apply L2-decay
+    input_img_data *= (1 - l2_decay)
 
-            # Apply gradient to image
-            input_img_data += grads_value * learning_rate
+    # apply Gaussian blur
+    if blur_every is not 0 and blur_std > 0:
+        if not iteration % blur_every:
+            # define standard deviations for blur kernel
+            blur_kernel_std = [0, blur_std, blur_std, blur_std]
+            
+            # blur along height and width, but not channels (colors)
+            blur_kernel_std[color_axis] = 0
+            
+            # perform blurring
+            input_img_data = gaussian_filter(input_img_data, sigma=blur_kernel_std)
 
-            if regularize:
-                # apply L2-regularizer
-                input_img_data *= (1 - l2_decay)
+    if value_percentile > 0:
+        small_entries = (abs(input_img_data) < np.percentile(abs(input_img_data), value_percentile))
+        input_img_data -= input_img_data * small_entries  # e.g. set smallest 50% of input_img_data to zero
 
-                # apply Gaussian blur
-                if not i % blur_freq:
-                    # blur along height and width, but not channels (colors)
-                    input_img_data = gaussian_filter(input_img_data, sigma=[0, 0, blur_std, blur_std])
+        # alternative approach
+        # get absolute values
+        abs_input_img_data = abs(input_img_data)
+        # find mask of high values (values above chosen value percentile)
+        high_value_mask = abs_input_img_data >= np.percentile(abs_input_img_data, value_percentile)
+        # apply to image remove small values
+        # input_img_data *= high_value_mask
+        alt_img = input_img_data * high_value_mask
+        
+        for i in range(len(input_img_data[0])):
+            for j in range(len(input_img_data[0, i])):
+                for k in range(len(input_img_data[0, i, j])):
+                    if input_img_data[0, i, j, k] != alt_img[0, i, j, k]:
+                        print('\n\nVALUE NOT EQUAL\n\n')
+        print('All is well with value')
 
-            # print('Current loss value:', loss_value)
-            print('Round %d finished.' % i)
+    if norm_percentile > 0:
+        # pxnorms = np.linalg.norm(input_img_data, axis=1)
+        pxnorms = np.linalg.norm(input_img_data, axis=3)
+        smallpx = pxnorms < np.percentile(pxnorms, norm_percentile)
+        # smallpx3 = np.tile(smallpx[:, np.newaxis, :, :], (1, 3, 1, 1))
+        smallpx3 = np.tile(smallpx[:, :, :, np.newaxis], (1, 1, 1, 3))
+        input_img_data -= input_img_data * smallpx3
 
-    # decode the resulting input image and add it to the list
-    img = deprocess(input_img_data[0])
-    kept_images.append((img, loss_value))
-    end_time = time.time()
-    print('Filter %d processed in %ds' % (class_index, end_time - start_time))
+        # alternative approach
+        # compute pixel norms along channel (color) axis
+        pixel_norms = np.linalg.norm(input_img_data, axis=color_axis)
+        
+        # find initial mask of high norms (norms above chosen norm percentile)
+        high_norm_mask = pixel_norms >= np.percentile(pixel_norms, norm_percentile)
+        
+        # expand mask to account for channels (colors)
+        high_norm_mask = expand_for_color(high_norm_mask, color_axis)
+        
+        # input_img_data *= high_norm_mask
+        alt_img = input_img_data * high_norm_mask
 
-# Compute the size of the grid
-n = int(np.ceil(np.sqrt(len(kept_images))))
+        for i in range(len(input_img_data[0])):
+            for j in range(len(input_img_data[0, i])):
+                for k in range(len(input_img_data[0, i, j])):
+                    if input_img_data[0, i, j, k] != alt_img[0, i, j, k]:
+                        print('\n\nNORM NOT EQUAL\n\n')
+        print('All is well with norm')
+        
+    if contribution_percentile > 0:
+        pred_0_contribution = grads_value * -input_img_data
+        contribution = pred_0_contribution.sum(1)  # sum over color channels
+        smallben = contribution < np.percentile(contribution, contribution_percentile)
+        smallben3 = np.tile(smallben[:, np.newaxis, :, :], (1, 3, 1, 1))
+        input_img_data -= input_img_data * smallben3
+    
+        # alternative approach
+        pred_0_contribution = -input_img_data * grads_value
+    
+        # sum over color channels
+        contribution = pred_0_contribution.sum(color_axis)
+        high_abs_contribution_mask = contribution >= np.percentile(contribution, contribution_percentile)
 
-# build a black picture with enough space for the kept_images
-# img_height = model.input_shape[2]
-# img_width = model.input_shape[3]
-img_height = model.input_shape[1]
-img_width = model.input_shape[2]
-margin = 5
-height = n * img_height + (n - 1) * margin
-width = n * img_width + (n - 1) * margin
-stitched_res = np.zeros((height, width, 3))
+        # expand mask to account for channels (colors)
+        high_abs_contribution_mask = expand_for_color(high_abs_contribution_mask, color_axis)
+        
+        # input_img_data *= high_abs_contribution_mask
+        alt_img = input_img_data * high_abs_contribution_mask
 
-# fill the picture with our saved filters
-for i in range(n):
-    for j in range(n):
-        if len(kept_images) <= i * n + j:
-            break
-        img, loss = kept_images[i * n + j]
-        stitched_res[(img_height + margin) * i: (img_height + margin) * i + img_height,
-        (img_width + margin) * j: (img_width + margin) * j + img_width, :] = img
+        for i in range(len(input_img_data[0])):
+            for j in range(len(input_img_data[0, i])):
+                for k in range(len(input_img_data[0, i, j])):
+                    if input_img_data[0, i, j, k] != alt_img[0, i, j, k]:
+                        print('\n\nCONTR. NOT EQUAL\n\n')
+        print('All is well with contr.')
 
-# save the result to disk
+    if abs_contribution_percentile > 0:
+        pred_0_contribution = grads_value * -input_img_data
+        contribution = pred_0_contribution.sum(1)  # sum over color channels
+        smallaben = abs(contribution) < np.percentile(abs(contribution), abs_contribution_percentile)
+        smallaben3 = np.tile(smallaben[:, np.newaxis, :, :], (1, 3, 1, 1))
+        input_img_data -= input_img_data * smallaben3
+    
+        # alternative approach
+        pred_0_contribution = -input_img_data * grads_value
+    
+        # sum over color channels
+        abs_contribution = abs(pred_0_contribution.sum(color_axis))
+        high_abs_contribution_mask = abs_contribution >= np.percentile(abs_contribution, abs_contribution_percentile)
 
-result_name = 'output/'
-if regularize:
-    result_name += 'regularized_lr=%s_iter=%s_l2=%s_bf=%s_bstd=%s.png' \
-                   % (str(learning_rate), str(no_of_iterations), str(l2_decay).replace('.', '-'),
-                      str(blur_freq), str(blur_std).replace('.', '-'))
-else:
-    result_name += 'vanilla_lr=%s_iter=%s.png' % (str(learning_rate).replace('.', '-'), str(no_of_iterations))
+        # expand mask to account for channels (colors)
+        high_abs_contribution_mask = expand_for_color(high_abs_contribution_mask, color_axis)
+        
+        # input_img_data *= high_abs_contribution_mask
+        alt_img = input_img_data * high_abs_contribution_mask
 
-scipy.misc.toimage(stitched_res, cmin=0, cmax=255).save(result_name)
-# Do not use scipy.misc.imsave because it will normalize the image pixel value between 0 and 255
+        for i in range(len(input_img_data[0])):
+            for j in range(len(input_img_data[0, i])):
+                for k in range(len(input_img_data[0, i, j])):
+                    if input_img_data[0, i, j, k] != alt_img[0, i, j, k]:
+                        print('\n\nABS CONTR. NOT EQUAL\n\n')
+        print('All is well with abs contr.')
+        
+    return input_img_data
+
+
+# use to expand a [batch, height, width]-numpy array with a color dimension
+def expand_for_color(np_array, color_axis):
+    if color_axis == 1:
+        # for numpy arrays on form [batch, color, height, width]
+        np_array = np.tile(np_array[:, np.newaxis, :, :], (1, 3, 1, 1))
+    elif color_axis == 3:
+        # for numpy arrays on form [batch, height, width, color]
+        np_array = np.tile(np_array[:, :, :, np.newaxis], (1, 1, 1, 3))
+    else:
+        raise ValueError('Color axis %d not recognized as legal axis' % color_axis)
+    return np_array
+
+
+def main():
+    # Creates the VGG model
+    model = create_model()
+    
+    # List of the generated images after learning
+    kept_images = []
+    
+    for class_index in [130, 351, 736, 850]:  # 130 flamingo, 351 hartebeest, 736 pool table, 850 teddy bear
+        print('Processing filter %d' % class_index)
+        start_time = time()
+        
+        iterate = get_gradient_function(model.input, model.output, class_index)
+    
+        # get initial image and its color axis
+        input_img_data, color_axis = create_initial_image(model.input_shape)
+        
+        loss_value = None
+        if class_index == 736:
+            # run gradient ascent for n steps
+            for i in range(no_of_iterations):
+                # input 0 for test phase
+                loss_value, grads_value = iterate([input_img_data, 0])
+        
+                # Apply gradient to image
+                input_img_data += grads_value * learning_rate
+        
+                if regularize:
+                    input_img_data = ensemble_regularization(input_img_data, grads_value, i, color_axis)
+        
+                # print('Current loss value:', loss_value)
+                print('Round %d finished.' % i)
+
+        # decode the resulting input image and add it to the list
+        img = deprocess(input_img_data, color_axis)
+        kept_images.append((img, loss_value))
+        print('Filter %d processed in %ds' % (class_index, time() - start_time))
+    
+    save_visualization(model.input_shape, kept_images)
+
+main()

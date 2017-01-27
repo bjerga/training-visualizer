ed3a3570dc75a318bc143eb1206d67d26ede7480
mikaelbj <mikaelbj@stud.ntnu.no>
2017-01-27 11:44:06 +0100

    Fix error where original approach updates image before alternate approach uses it

--- test_code/deep_dream/deep_vis_keras.py
+++ test_code/deep_dream/deep_vis_keras.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 import scipy.misc
-from time import time
 from scipy.ndimage.filters import gaussian_filter
 from os import mkdir
 from os.path import dirname, join
+from time import time
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -15,21 +15,22 @@ from keras.applications.vgg16 import VGG16
 MEAN_VALUES = np.array([103.939, 116.779, 123.68])
 
 # Update coefficient
-learning_rate = 1000.
+learning_rate = 4000.
 
 # how many times we update image
 no_of_iterations = 500
 
 # specify L2-decay
 # used to prevent a small number of extreme pixel values from dominating the output image
-l2_decay = 0.0001
+# l2_decay = 0.0001
+l2_decay = 0.0
 
 # specify frequency of blurring and standard deviation for kernel for Gaussian blur
 # used to penalize high frequency information in the output image
 blur_every = 4
 # standard deviation values between 0.0 and 0.3 work poorly, according to yosinski
-# blur_std = 0.3
-blur_std = 1.0
+# blur_std = 1.0
+blur_std = 0.5
 
 # specify value percentile limit
 # used to induce sparsity by setting pixels with small absolute value to 0
@@ -37,7 +38,7 @@ value_percentile = 0
 
 # specify norm percentile limit
 # used to induce sparsity by setting pixels with small norm to 0
-norm_percentile = 0
+norm_percentile = 50
 
 # specify contribution percentile limit
 # used to induce sparsity by setting pixels with small contribution to 0
@@ -167,16 +168,18 @@ def create_initial_image(model_input_shape):
     np.random.seed(1337)  # for reproducibility
     # we start from a gray image with some random noise
     
-    # return a random, intial image, and the color axis of the image
+    # return a random, initial image, and the color axis of the image
     # add (1,) for batch axis
     return np.random.normal(0, 10, (1,) + model_input_shape[1:]), model_input_shape.index(3)
 
 
 def ensemble_regularization(input_img_data, grads_value, iteration, color_axis):
     
+    # regularizer #1
     # apply L2-decay
     input_img_data *= (1 - l2_decay)
 
+    # regularizer #2
     # apply Gaussian blur
     if blur_every is not 0 and blur_std > 0:
         if not iteration % blur_every:
@@ -189,19 +192,25 @@ def ensemble_regularization(input_img_data, grads_value, iteration, color_axis):
             # perform blurring
             input_img_data = gaussian_filter(input_img_data, sigma=blur_kernel_std)
 
+    # regularizer #3
+    # apply value limit
     if value_percentile > 0:
-        small_entries = (abs(input_img_data) < np.percentile(abs(input_img_data), value_percentile))
-        input_img_data -= input_img_data * small_entries  # e.g. set smallest 50% of input_img_data to zero
-
+        
         # alternative approach
         # get absolute values
         abs_input_img_data = abs(input_img_data)
+        
         # find mask of high values (values above chosen value percentile)
         high_value_mask = abs_input_img_data >= np.percentile(abs_input_img_data, value_percentile)
+        
         # apply to image remove small values
         # input_img_data *= high_value_mask
         alt_img = input_img_data * high_value_mask
         
+        # original approach
+        small_entries = (abs(input_img_data) < np.percentile(abs(input_img_data), value_percentile))
+        input_img_data -= input_img_data * small_entries  # e.g. set smallest 50% of input_img_data to zero
+
         for i in range(len(input_img_data[0])):
             for j in range(len(input_img_data[0, i])):
                 for k in range(len(input_img_data[0, i, j])):
@@ -209,13 +218,9 @@ def ensemble_regularization(input_img_data, grads_value, iteration, color_axis):
                         print('\n\nVALUE NOT EQUAL\n\n')
         print('All is well with value')
 
+    # regularizer #4
+    # apply norm limit
     if norm_percentile > 0:
-        # pxnorms = np.linalg.norm(input_img_data, axis=1)
-        pxnorms = np.linalg.norm(input_img_data, axis=3)
-        smallpx = pxnorms < np.percentile(pxnorms, norm_percentile)
-        # smallpx3 = np.tile(smallpx[:, np.newaxis, :, :], (1, 3, 1, 1))
-        smallpx3 = np.tile(smallpx[:, :, :, np.newaxis], (1, 1, 1, 3))
-        input_img_data -= input_img_data * smallpx3
 
         # alternative approach
         # compute pixel norms along channel (color) axis
@@ -230,32 +235,46 @@ def ensemble_regularization(input_img_data, grads_value, iteration, color_axis):
         # input_img_data *= high_norm_mask
         alt_img = input_img_data * high_norm_mask
 
+        # original approach
+        # pxnorms = np.linalg.norm(input_img_data, axis=1)
+        pxnorms = np.linalg.norm(input_img_data, axis=3)
+        smallpx = pxnorms < np.percentile(pxnorms, norm_percentile)
+        # smallpx3 = np.tile(smallpx[:, np.newaxis, :, :], (1, 3, 1, 1))
+        smallpx3 = np.tile(smallpx[:, :, :, np.newaxis], (1, 1, 1, 3))
+        input_img_data -= input_img_data * smallpx3
+
         for i in range(len(input_img_data[0])):
             for j in range(len(input_img_data[0, i])):
                 for k in range(len(input_img_data[0, i, j])):
                     if input_img_data[0, i, j, k] != alt_img[0, i, j, k]:
                         print('\n\nNORM NOT EQUAL\n\n')
         print('All is well with norm')
-        
+
+    # regularizer #5
+    # apply contribution limit
     if contribution_percentile > 0:
-        pred_0_contribution = grads_value * -input_img_data
-        contribution = pred_0_contribution.sum(1)  # sum over color channels
-        smallben = contribution < np.percentile(contribution, contribution_percentile)
-        smallben3 = np.tile(smallben[:, np.newaxis, :, :], (1, 3, 1, 1))
-        input_img_data -= input_img_data * smallben3
     
         # alternative approach
         pred_0_contribution = -input_img_data * grads_value
     
         # sum over color channels
         contribution = pred_0_contribution.sum(color_axis)
-        high_abs_contribution_mask = contribution >= np.percentile(contribution, contribution_percentile)
+        high_contribution_mask = contribution >= np.percentile(contribution, contribution_percentile)
 
         # expand mask to account for channels (colors)
-        high_abs_contribution_mask = expand_for_color(high_abs_contribution_mask, color_axis)
+        high_contribution_mask = expand_for_color(high_contribution_mask, color_axis)
         
-        # input_img_data *= high_abs_contribution_mask
-        alt_img = input_img_data * high_abs_contribution_mask
+        # input_img_data *= high_contribution_mask
+        alt_img = input_img_data * high_contribution_mask
+        
+        # original approach
+        pred_0_contribution = grads_value * -input_img_data
+        # contribution = pred_0_contribution.sum(1)  # sum over color channels
+        contribution = pred_0_contribution.sum(3)  # sum over color channels
+        smallben = contribution < np.percentile(contribution, contribution_percentile)
+        # smallben3 = np.tile(smallben[:, np.newaxis, :, :], (1, 3, 1, 1))
+        smallben3 = np.tile(smallben[:, :, :, np.newaxis], (1, 1, 1, 3))
+        input_img_data -= input_img_data * smallben3
 
         for i in range(len(input_img_data[0])):
             for j in range(len(input_img_data[0, i])):
@@ -264,12 +283,9 @@ def ensemble_regularization(input_img_data, grads_value, iteration, color_axis):
                         print('\n\nCONTR. NOT EQUAL\n\n')
         print('All is well with contr.')
 
+    # regularizer #6
+    # apply absolute contribution limit
     if abs_contribution_percentile > 0:
-        pred_0_contribution = grads_value * -input_img_data
-        contribution = pred_0_contribution.sum(1)  # sum over color channels
-        smallaben = abs(contribution) < np.percentile(abs(contribution), abs_contribution_percentile)
-        smallaben3 = np.tile(smallaben[:, np.newaxis, :, :], (1, 3, 1, 1))
-        input_img_data -= input_img_data * smallaben3
     
         # alternative approach
         pred_0_contribution = -input_img_data * grads_value
@@ -283,6 +299,15 @@ def ensemble_regularization(input_img_data, grads_value, iteration, color_axis):
         
         # input_img_data *= high_abs_contribution_mask
         alt_img = input_img_data * high_abs_contribution_mask
+
+        # original approach
+        pred_0_contribution = grads_value * -input_img_data
+        # contribution = pred_0_contribution.sum(1)  # sum over color channels
+        contribution = pred_0_contribution.sum(3)  # sum over color channels
+        smallaben = abs(contribution) < np.percentile(abs(contribution), abs_contribution_percentile)
+        # smallaben3 = np.tile(smallaben[:, np.newaxis, :, :], (1, 3, 1, 1))
+        smallaben3 = np.tile(smallaben[:, :, :, np.newaxis], (1, 1, 1, 3))
+        input_img_data -= input_img_data * smallaben3
 
         for i in range(len(input_img_data[0])):
             for j in range(len(input_img_data[0, i])):
@@ -308,16 +333,20 @@ def expand_for_color(np_array, color_axis):
 
 
 def main():
-    # Creates the VGG model
+    # create model to generate gradients from
     model = create_model()
     
-    # List of the generated images after learning
-    kept_images = []
-    
-    for class_index in [130, 351, 736, 850]:  # 130 flamingo, 351 hartebeest, 736 pool table, 850 teddy bear
-        print('Processing filter %d' % class_index)
+    # list of images generated from regularized gradient ascent
+    generated_images = []
+
+    # 130 flamingo, 351 hartebeest, 736 pool table, 850 teddy bear
+    for class_index in [130, 351, 736, 850]:
+        print('Processing class %d' % class_index)
+        
+        # used to time generation of each image
         start_time = time()
         
+        # create and save gradient function for current class
         iterate = get_gradient_function(model.input, model.output, class_index)
     
         # get initial image and its color axis
@@ -325,8 +354,10 @@ def main():
         
         loss_value = None
         if class_index == 736:
+        # if class_index:
             # run gradient ascent for n steps
             for i in range(no_of_iterations):
+                
                 # input 0 for test phase
                 loss_value, grads_value = iterate([input_img_data, 0])
         
@@ -341,9 +372,9 @@ def main():
 
         # decode the resulting input image and add it to the list
         img = deprocess(input_img_data, color_axis)
-        kept_images.append((img, loss_value))
+        generated_images.append((img, loss_value))
         print('Filter %d processed in %ds' % (class_index, time() - start_time))
     
-    save_visualization(model.input_shape, kept_images)
+    save_visualization(model.input_shape, generated_images)
 
 main()

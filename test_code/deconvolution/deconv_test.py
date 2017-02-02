8d2f4de787e45a970011c1d942f6830979744731
mikaelbj <mikaelbj@stud.ntnu.no>
2017-02-02 16:34:24 +0100

    Add initial attempt at unpooling a max pooling layer output

---
+++ test_code/deconvolution/deconv_test.py
@@ -0,0 +1,196 @@
+import numpy as np
+
+import keras.backend as K
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D
+
+
+# må finne måte å implementere max switch på
+# kan potensielt bruke samme stil som for å hente alle activations, med K-funksjon
+
+
+def simple_example():
+	shape = (4, 4, 1)
+	
+	img = np.random.randint(0, 10, (1,) + shape)
+	
+	model = Sequential()
+	model.add(MaxPooling2D((2, 2), input_shape=shape))
+	
+	model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+	
+	pred = model.predict(img)
+	
+	print('Original:')
+	array_print(img)
+	
+	print('Processed:')
+	array_print(pred)
+
+
+def adv_example():
+	shape = (6, 6, 1)
+	
+	img = np.random.randint(0, 10, (1,) + shape)
+	
+	model = Sequential()
+	model.add(Convolution2D(1, 2, 2, input_shape=shape))
+	model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+	
+	model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+	
+	pred = model.predict(img)
+	
+	print('Original:')
+	array_print(img)
+	
+	print('Processed:')
+	print(pred)
+
+	get_activation_tensor = K.function([model.input, K.learning_phase()], [model.layers[0].output])
+	temp = get_activation_tensor([img, 0])[0]
+	print('\n', model.layers[0].name)
+	print('Shape:', temp.shape)
+	array_print(temp)
+	
+	# print('Pure:', temp)
+	
+	get_activation_tensor = K.function([model.layers[1].input, K.learning_phase()], [model.layers[1].output])
+	temp2 = get_activation_tensor([temp, 0])[0]
+	print('\n', model.layers[1].name)
+	print('Shape:', temp2.shape)
+	array_print(temp2)
+	
+	# print(model.layers[1].get_config())
+	# print(model.layers[1].pool_size)
+	
+	runs = 1
+	test_speed = False
+	if test_speed:
+		runs = 1000000
+	
+	for _ in range(runs):
+		unpooled_alt1 = find_max_pos(temp, model.layers[1].pool_size)
+	print('\nUnpooled alternative 1:')
+	array_print(unpooled_alt1)
+
+	for _ in range(runs):
+		unpooled_alt2 = unpool_with_mask(temp, temp2, model.layers[1].get_config())
+	print('\nUnpooled alternative 2:')
+	array_print(unpooled_alt2)
+	
+	
+# TODO: add support for channels, strides and border modes
+def unpool_with_mask(pool_input, pool_output, pool_config):
+	
+	# print(pool_config)
+	
+	output = np.empty(pool_input.shape)
+	output[:] = None
+	
+	# unpool_mask = np.repeat(np.repeat(pool_output, pool_size[0], axis=1), pool_size[1], axis=2)
+	
+	unpool_mask = pool_output
+	for i in range(len(pool_config['pool_size'])):
+		unpool_mask = np.repeat(unpool_mask, pool_config['pool_size'][i], axis=i+1)
+
+	# print('\nUnpool mask after step 1:')
+	# array_print(unpool_mask)
+	
+	# if valid pooling is used, some of the outermost pixel might have been lost in pooling
+	# if so, add None's to match pooling input shape
+	# print('Shapes:', pool_input.shape, unpool_mask.shape)
+	# axis = 1
+	# while pool_input.shape != unpool_mask.shape:
+		# unpool_mask = np.insert(unpool_mask, unpool_mask.shape[axis], None, axis=axis)
+		# print('\nUpdated shapes:', pool_input.shape, unpool_mask.shape)
+		# array_print(unpool_mask)
+		# if axis == 1:
+		# 	axis = 2
+		# else:
+		# 	axis = 1
+	
+	if pool_input.shape != unpool_mask.shape:
+		output[:, :unpool_mask.shape[1], :unpool_mask.shape[2], :] = unpool_mask
+		unpool_mask = output
+	
+	unpool_mask = unpool_mask == pool_input
+	# print('\nUnpool mask after step 3:', unpool_mask)
+	# array_print(unpool_mask)
+	
+	return pool_input * unpool_mask
+	
+	
+def find_max_pos(input_tensor, pool_size):
+	
+	# print('\nINPUT INFO:')
+	# print(len(input_tensor))
+	# print(len(input_tensor[0]))
+	# print(len(input_tensor[0, 0]))
+	# print(len(input_tensor[0, 0, 0]))
+	#
+	# print(pool_size[0])
+	# print(pool_size[1])
+	
+	unpooled = np.zeros(input_tensor.shape)
+	
+	# print('\nMax. pos.:')
+	for i in range(0, len(input_tensor[0]), pool_size[0]):
+		for j in range(0, len(input_tensor[0, 0]), pool_size[1]):
+			if i+pool_size[0] <= len(input_tensor[0, 1]) and j+pool_size[1] <= len(input_tensor[0, 2]):
+				
+				quadrant = input_tensor[0, i:i+pool_size[0], j:j+pool_size[1], 0]
+				quad_max_pos = np.array(np.unravel_index(np.argmax(quadrant), quadrant.shape))
+				full_max_pos = quad_max_pos + [i, j]
+				
+				# print('Make quadrant from [%d:%d] to [%d:%d]' % (i, i+pool_size[0], j, j+pool_size[1]))
+				# print('Equivalent to quadrant staring at (%d,%d) and ending at (%d,%d)' % (i, j, i+pool_size[0]-1, j+pool_size[1]-1))
+				# print(quadrant)
+				# print('Quad. max.:', np.amax(quadrant))
+				# print('Quad. max. pos.:', np.unravel_index(np.argmax(quadrant), quadrant.shape))
+				# print('Equivalent full max. pos.:', max_pos)
+				# print('i:', i, ', j:', j, '\n')
+				
+				unpooled[0, full_max_pos[0], full_max_pos[1], 0] = quadrant[quad_max_pos[0], quad_max_pos[1]]
+				
+	return unpooled
+
+
+def array_print(np_array):
+	for i in range(len(np_array[0])):
+		row = ''
+		for j in range(len(np_array[0, i])):
+			row += ' ' + str(np_array[0, i, j, 0])
+		print(row)
+
+
+def main():
+	print('SIMPLE')
+	simple_example()
+	
+	print('\n\nADVANCED')
+	adv_example()
+
+main()
+	
+	
+
+
+# class Unpooling2D(Layer):
+#     def __init__(self, poolsize=(2, 2), ignore_border=True):
+#         super(Unpooling2D,self).__init__()
+#         self.input = T.tensor4()
+#         self.poolsize = poolsize
+#         self.ignore_border = ignore_border
+#
+#     def get_output(self, train):
+#         X = self.get_input(train)
+#         s1 = self.poolsize[0]
+#         s2 = self.poolsize[1]
+#         output = X.repeat(s1, axis=2).repeat(s2, axis=3)
+#         return output
+#
+#     def get_config(self):
+#         return {"name":self.__class__.__name__,
+#             "poolsize":self.poolsize,
+#             "ignore_border":self.ignore_border}

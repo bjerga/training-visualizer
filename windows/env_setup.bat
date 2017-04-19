14e0512cbc62f3f6c1545c969463756fe28c4310
mikaelbj <mikaelbj@stud.ntnu.no>
2017-04-19 12:42:38 +0200

    Add fix that prevents adding windows-folder to PYTHONPATH

--- windows/env_setup.bat
+++ windows/env_setup.bat
@@ -1,5 +1,6 @@
 @echo off
 set FLASK_APP=visualizer
 echo Flask app set to visualizer
-set PYTHONPATH=%PYTHONPATH%;%~dp0
+set current_dir=%~dp0
+set PYTHONPATH=%PYTHONPATH%;%current_dir:~0,-8%
 echo PYTHONPATH set to %PYTHONPATH%

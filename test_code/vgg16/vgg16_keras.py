e3464a6e98a65e13e33b69e8fcaa9ba3cd81ad43
mikaelbj <mikaelbj@stud.ntnu.no>
2017-04-27 15:41:18 +0200

    Add initial test code for new toy problem: VGG16 in Keras

--- test_code/vgg16/vgg16_keras.py
+++ test_code/vgg16/vgg16_keras.py
@@ -5,12 +5,12 @@ from os.path import join, dirname
 
 import numpy as np
 
+import keras.backend as K
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 
 # import callbacks for visualizing
-from visualizer.callbacks import NetworkSaver, AccuracyListSaver, LossListSaver, ActivationTupleListSaver, SaliencyMaps, \
-	DeepVisualization, Deconvolution
+from custom_keras.callbacks import NetworkSaver, TrainingProgress, LayerActivations, SaliencyMaps, DeepVisualization, Deconvolution
 
 # find path to save networks and results
 save_path = dirname(__file__)
@@ -24,13 +24,14 @@ val_data_path = join(imagenet_path, 'val_data')
 train_data_names = listdir(train_data_path)
 train_data_amount = len(train_data_names)
 
-with open(join(imagenet_path, 'wnid_index_map.pickle')) as f:
+with open(join(imagenet_path, 'wnid_index_map.pickle'), 'rb') as f:
 	wnid_index_map = pickle.load(f)
 
 
 def create_model(input_shape):
 	# define model
-	model = VGG16(include_top=True, weights='imagenet', input_shape=input_shape)
+	# model = VGG16(include_top=True, input_shape=input_shape)
+	model = VGG16()
 	model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
 	
 	print('\nModel successfully created')
@@ -43,24 +44,19 @@ def train(model, no_of_epochs=10):
 	print('\n\nCommence VGG16 model training\n')
 	
 	# initialize custom callbacks
-	custom_callbacks = [NetworkSaver(save_path), AccuracyListSaver(save_path), LossListSaver(save_path),
-						SaliencyMaps(save_path),
-						DeepVisualization(save_path,
-										  [(-1, 0), (-1, 1), (-1, 2), (-1, 3), (-1, 4), (-1, 5), (-1, 6), (-1, 7),
-										   (-1, 8), (-1, 9)],
-										  2500.0, 500, l2_decay=0.0001, blur_interval=4, blur_std=1.0),
-						Deconvolution(save_path, feat_map_layer_no=3, feat_map_amount=3)]
+	# neurons_to_visualize = [(-1, 0), (-1, 1), (-1, 2)]
+	# custom_callbacks = [NetworkSaver(save_path), TrainingProgress(save_path), LayerActivations(save_path), SaliencyMaps(save_path),
+	# 					DeepVisualization(save_path, neurons_to_visualize,
+	# 									  2500.0, 500, l2_decay=0.0001, blur_interval=4, blur_std=1.0),
+	# 					Deconvolution(save_path, feat_map_layer_no=3, feat_map_amount=3)]
 	
-	batch_size = 256
+	# batch_size = 256
+	batch_size = 2
 	steps_per_epoch = train_data_amount / batch_size
 	
 	print('Steps per epoch:', steps_per_epoch)
 	
-	my_gen = data_generator()
-	
-	
-	# train with chosen hyperparameters
-	model.fit_generator(generator=my_gen, steps_per_epoch=steps_per_epoch, epochs=30, verbose=1, )
+	model.fit_generator(generator=data_generator(batch_size), steps_per_epoch=steps_per_epoch, epochs=30, verbose=1)
 	
 	print('\nCompleted VGG16 model training\n')
 	
@@ -68,23 +64,44 @@ def train(model, no_of_epochs=10):
 
 def data_generator(batch_size):
 	
+	zeroes = np.zeros((1, 1000))
 	while True:
 		random_img_names = np.random.choice(train_data_names, batch_size, replace=False)
+		for img_name in random_img_names:
 		
-		x_data = [preprocess_data(load_image(train_data_path, img_name)) for img_name in random_img_names]
-		y_data = [wnid_index_map.index(img_name) for img_name in random_img_names]
-		
-		yield x_data, y_data
+			# x_data = np.array([preprocess_data(load_image(train_data_path, img_name), (224, 224)) for img_name in random_img_names])
+			x_data = preprocess_data(load_image(train_data_path, img_name), (224, 224))
+			x_data = np.expand_dims(x_data, 0)
+			# y_data = [wnid_index_map[img_name] for img_name in random_img_names]
+			y_data = zeroes.copy()
+			y_data[0][42] = 1.0
+
+			yield x_data, y_data
 
 def load_image(img_path, img_name):
 	img = image.load_img(join(img_path, img_name))
 	return image.img_to_array(img)
 
-def preprocess_data(img_array):
+def preprocess_data(img_array, target_size, mean_values=np.array([103.939, 116.779, 123.68])):
+	
+	# change size of image
+	img = image.array_to_img(img_array)
+	height_weight_tuple = (target_size[1], target_size[0])
+	if img.size != height_weight_tuple:
+		img = img.resize(height_weight_tuple)
+		img_array = image.img_to_array(img)
+	
+	# subtract mean values
+	if K.image_data_format() == 'channels_last':
+		img_array -= mean_values.reshape((1, 1, 3))
+	else:
+		img_array -= mean_values.reshape((3, 1, 1))
+	
 	return img_array
 
 
-def postprocess_data(img_array):
+def postprocess_data(img_array, mean_values=np.array([103.939, 116.779, 123.68])):
+	img_array += mean_values.reshape((1, 1, 3))
 	return img_array
 
 
@@ -93,7 +110,12 @@ def main():
 	
 	start_time = time()
 	
-	model = create_model()
+	if K.image_data_format() == 'channels_last':
+		input_shape = (224, 224, 3)
+	else:
+		input_shape = (3, 224, 224)
+
+	model = create_model(input_shape)
 	
 	model = train(model)
 	

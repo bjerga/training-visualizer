67b9de29e6114508d7bf9dcb09f160d232a211e5
Annie Aasen <aasenannie@gmail.com>
2017-04-27 12:31:55 +0200

    Add deconvolution visualization to tab

--- bokeh.sh
+++ bokeh.sh
@@ -3,4 +3,5 @@ bokeh serve --allow-websocket-origin=localhost:5000 \
 	custom_bokeh/training_progress.py \
 	custom_bokeh/layer_activations.py \
 	custom_bokeh/saliency_maps.py \
-	custom_bokeh/deep_visualization.py
+	custom_bokeh/deep_visualization.py \
+	custom_bokeh/deconvolution.py

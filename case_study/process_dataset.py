4c01d6defbb0efa4fe98c7fc91a70275fb3c5819
Annie Aasen <aasenannie@gmail.com>
2017-05-11 15:29:18 +0200

    Pad and resize all images

--- case_study/process_dataset.py
+++ case_study/process_dataset.py
@@ -2,6 +2,7 @@ import os
 import glob
 import pickle
 from PIL import Image
+import math
 
 root = '/Users/annieaa/Documents/NTNU/Fordypningsprosjekt/IMFDB'
 
@@ -51,6 +52,72 @@ print(len(training_data))
 
 #with open(os.path.join('/Users/annieaa/Documents/NTNU/Fordypningsprosjekt/visualizer/case_study', 'imfdb_training_data.pickle'), 'wb') as f:
 #	pickle.dump(training_data, f)
+
+# AVERAGES:
+
+# print(widths/number_of_images)  # 101.85763185005483
+# print(heights/number_of_images)  # 128.47163603735336
+# print(min_width)  # 8
+# print(min_height)  # 11
+# print(max_width)  # 852
+# print(max_height)  # 964
+
+
+base_url = '/Users/annieaa/Documents/NTNU/Fordypningsprosjekt'
+
+with open(os.path.join(base_url, 'imfdb_training_data.pickle'), 'rb') as f:
+	training_data = pickle.load(f)
+
+img_size = (130, 130)
+
+
+def get_averages():
+
+	number_of_images = 0
+	widths = 0
+	heights = 0
+	min_width = 10000
+	min_height = 10000
+	max_width = 0
+	max_height = 0
+
+	for x in training_data:
+		number_of_images += 1
+		image = Image.open(os.path.join(base_url, x[0]))
+		width, height = image.size
+
+		if height < min_height:
+			min_height = height
+		if width < min_width:
+			min_width = width
+		if height > max_height:
+			max_height = height
+		if width > max_width:
+			max_width = width
+
+		widths += width
+		heights += height
+
+
+def pad_images():
+	for x in training_data:
+		path = os.path.join(base_url, x[0])
+		image = Image.open(path)
+		width, height = image.size
+
+		if width < height:
+			new_width = math.ceil(img_size[1] * width / height)
+			image = image.resize((new_width, img_size[1]), Image.ANTIALIAS)
+		elif width > height:
+			new_height = math.ceil(img_size[0] * height / width)
+			image = image.resize((img_size[0], new_height), Image.ANTIALIAS)
+		else:
+			image = image.resize(img_size)
+
+		new_image = Image.new('RGB', img_size)
+		new_image.paste(image, (math.ceil((img_size[0] - image.size[0])/2),
+								math.ceil((img_size[1] - image.size[1])/2)))
+		new_image.save(path)
 
 
 
